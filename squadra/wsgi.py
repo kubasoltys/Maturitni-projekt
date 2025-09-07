63e08c3b01668dcd281330cf0cdbcf3731cc3ef0
kubasoltys <jakub.soltys22@gmail.com>
2025-09-07 16:29:22 +0200

    django_zalezeni

---
+++ squadra/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for squadra project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'squadra.settings')
+
+application = get_wsgi_application()
