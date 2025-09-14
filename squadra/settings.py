405013e724b59c758796252e8c0aa601530436c0
kubasoltys <jakub.soltys22@gmail.com>
2025-09-14 23:05:25 +0200

    nova_databaze_postgresql_a_requirements.txt

--- squadra/settings.py
+++ squadra/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'squadra.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maturitni_projekt',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
